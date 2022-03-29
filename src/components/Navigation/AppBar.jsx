41812f7e31ecd7d8a856483011df4fb1d1d22018
Zidan Omar Hamid <hzidanomar@gmail.com>
2022-03-29 07:45:43 +0300

    setup material ui and project folder structures

---
+++ src/components/Navigation/AppBar.jsx
@@ -0,0 +1,106 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Box, Menu, MenuItem } from '@mui/material';
+
+import logo from '../../assets/images/logo.png';
+import PersonIcon from '@mui/icons-material/Person';
+
+const drawerWidth = 240;
+
+const Header = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+export default function AppBar({ open, handleDrawerOpen }) {
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <Header position='fixed' open={open} elevation={0} sx={{ display: 'flex' }}>
+      <Toolbar>
+        <IconButton
+          color='inherit'
+          aria-label='open drawer'
+          onClick={handleDrawerOpen}
+          edge='start'
+          sx={{
+            marginRight: 5,
+            ...(open && { display: 'none' }),
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Box
+          sx={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: open ? 'flex-end' : 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          {!open && (
+            <Box sx={{ height: '2rem', '& img': { height: '100%' } }}>
+              <img src={logo} alt='invision' />
+            </Box>
+          )}
+          {auth && (
+            <Box sx={{ justifySelf: 'end' }}>
+              <IconButton
+                size='large'
+                aria-label='account of current user'
+                aria-controls='menu-appbar'
+                aria-haspopup='true'
+                onClick={handleMenu}
+                color='inherit'
+              >
+                <PersonIcon />
+              </IconButton>
+              <Menu
+                id='menu-appbar'
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </Box>
+          )}
+        </Box>
+      </Toolbar>
+    </Header>
+  );
+}
---
+++ src/theme/index.js
@@ -0,0 +1,33 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          color: '#3F4C54',
+          backgroundColor: '#F5F5F9',
+        },
+      },
+    },
+  },
+  palette: {
+    common: {
+      black: '#3F4C54',
+    },
+    primary: {
+      main: '#91B7C2',
+    },
+    secondary: { main: '#FEAF66' },
+    pLight: '#ECF7FA',
+    surface: '#545F6E',
+    gray: '#A6AEBA',
+    bg: '#F5F5F9',
+  },
+  typography: {
+    fontFamily: ['Nunito', 'sans-serif'].join(','),
+    fontSize: 16,
+  },
+});
+
+export default theme;
